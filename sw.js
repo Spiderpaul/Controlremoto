efd5ac3fd2f5f6125f9ba8f83cb7574858d60074
Paul Lugo <paulugortega@gmail.com>
2026-05-27 13:46:49 -0700

    Subiendo prototipo

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Service Worker básico para cumplir los requisitos de instalación de la PWA
+self.addEventListener('fetch', (event) => {
+    // No hace nada por ahora, solo deja pasar las peticiones
+});
